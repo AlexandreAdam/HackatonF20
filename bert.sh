5f197aed52db4c216a3dc820c89adcbd7f676c75
AlexandreAdam <adam.alexandre01123@gmail.com>
2020-11-22 03:13:00 -0500

    Added cc job script

---
+++ bert.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+#SBATCH --account=def-lplevass
+#SBATCH --gres=gpu:2              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=6         # CPU cores/threads
+#SBATCH --mem=4000M               # memory per node
+#SBATCH --time=0-03:00            # time (DD-HH:MM)
+module load python/3.6
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/hackaton/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r requirement_beluga.txt
+python bert_features.py
