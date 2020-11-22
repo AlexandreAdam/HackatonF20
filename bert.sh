3205235ab631eb6b0940b05eb2faf0878348bdcf
AlexandreAdam <adam.alexandre01123@gmail.com>
2020-11-22 03:26:40 -0500

    slam fix in bert

--- bert.sh
+++ bert.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=0-03:00            # time (DD-HH:MM)
 module load python/3.6
 virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/hackaton/bin/activate
+source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 
 pip install --no-index -r requirement_beluga.txt
